3b581ae5dfa91165b628639a286c131a07a31578
narmit <narmitmashruwala@gmail.com>
2023-04-30 17:23:03 -0400

    chore: remove json file

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,9 +1,6 @@
 // #FIREBASEAUTH Need to install firebase-admin from npm
 import admin from "firebase-admin";
 
-// #FIREBASEAUTH service account file can be downloaded somewhere from firebase application console
-import serviceAccount from "./serviceAccounts.json" assert { type: "json" };
-
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 //   databaseURL: "https://synth-6f232-default-rtdb.firebaseio.com",
