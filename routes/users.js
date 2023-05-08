85adc3efca73b4d481fc495a9740eaab1b7d3bc7
narmit <narmitmashruwala@gmail.com>
2023-05-08 01:56:11 -0400

    fix

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,13 @@ import { usersDL } from "../data/index.js";
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(async (req, res, next) => {
+router.route("/").get(async (req, res, next) => {
+  try {
     const data = await usersDL.getUsers();
     return res.json(data);
-  })
-  .post(async (req, res) => {
-    return res.status(404).send("Not implemented");
-  });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
